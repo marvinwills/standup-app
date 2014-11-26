9a490d412a9ac1a51b636caf88da9bd8d548bedc
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-26 11:33:03 +0100

    Adding model.

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -7,8 +7,15 @@ class StandupsController < ApplicationController
 	end
 	
 	def create
+		@standup = Standup.New
+		@yesterday_item = @standup.yesterday_items.create(1, :yesterday)
+		@today_item = @standup.today_items.create(1, :today)
 
-		redirect_to standups_path
+		if @standup.save
+			redirect_to standups_path
+		else
+			render 'new'
+		end
 	end
 	
 	def edit
