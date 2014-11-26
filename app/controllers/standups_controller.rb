ad3f51287bdf4d0c89594b7d8e54a3a996c9181b
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-26 04:09:07 +0100

    Adding page placehodlers.

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -1,12 +1,21 @@
 class StandupsController < ApplicationController
 
-	def new
-	end
-	
 	def index
 	end
 	
+	def new
+	end
+	
+	def create
+	end
+	
 	def edit
+	end
+	
+	def update
+	end
+	
+	def destroy
 	end
 	
 end
