1c126c228df045d3b8c3f1054bcf16568f77aafb
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-26 17:18:15 +0100

    Adding forms.

--- app/models/standup.rb
+++ app/models/standup.rb
@@ -1,6 +1,6 @@
 class Standup < ActiveRecord::Base
 	has_many :yesterday_items, dependent: :destroy
 	has_many :today_items, dependent: :destroy
-	accepts_nested_attributes_for :today_items
-	accepts_nested_attributes_for :yesterday_items
+	accepts_nested_attributes_for :today_items, :reject_if => :all_blank, :allow_destroy => true
+	accepts_nested_attributes_for :yesterday_items, :reject_if => :all_blank, :allow_destroy => true
 end
