9335193dba0a4d3eb24458f56a33a120a5d8ab5e
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-27 10:51:04 +0100

    Adding authentiation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  #devise_for :installs
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
