81e5fa465e9b0b3e450f9c27075bae7bea6a13c8
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-27 15:49:22 +0100

    Adding authentiation.

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -1,9 +1,8 @@
 class StandupsController < ApplicationController
 
 	def index
-		@user = User.find(params[:user_id])
+		#@user = User.find(params[:user_id])
 		#@standups = @user.standups
-		@standups = Standup.all
 	end
 	
 	def new
