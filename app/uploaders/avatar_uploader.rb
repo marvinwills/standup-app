670b810b85cff3616934a2eb307acfe301b44f2f
pyromaniackeca <nemanja.keca@gmail.com>
2014-11-30 05:19:12 +0100

    Resolved heroku issues.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -19,6 +19,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
+  
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
